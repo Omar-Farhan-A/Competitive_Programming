0e39d52cd99b8bdf9883dc96e358cd5a5757103a
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-02-21 00:43:01 +0200

    Update Segment_Tree.cpp

--- Segment_Tree.cpp
+++ Segment_Tree.cpp
@@ -1,25 +1,3 @@
-#define ll long long
-#define isON(n, k) (((n) >> (k)) & 1)
-#define sz(x) int(x.size())
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-using namespace std;
-using namespace __gnu_pbds;
-template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
-template<class T> using  ordered_multiset= tree<pair<T, T>, null_type, less<pair<T, T>>, rb_tree_tag, tree_order_statistics_node_update>;
-const int N=1e5+7,OO=0x3f3f3f3f,M=1e3+5,MOD=1e9+7;
-int dx[]={1,0, -1,0,1,1,-1,-1};
-int dy[]={0,1, 0,-1,1,-1,1,-1};//
-template < typename T = int > using Pair = pair < T, T >;
-template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
-    for (auto &x: v) in >> x;
-    return in;
-}
-template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) {
-    for (const T &x: v) out << x<<' ' ;
-    return out;
-}
 vector<int>seg(4 * N);
 vector<int>nums;
 int merge(int a,int  b){

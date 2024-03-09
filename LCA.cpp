ed45a5e450964088a4d66262b5ff91dbb0e0906e
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-03-09 04:27:21 +0200

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,47 +1,34 @@
+
 vector<vector<int>>up;
 vector<int>adj[N],depth;
 int l;
-void dfs(int u,int p,int d){
-    depth[u]=d;
+void dfs(int u,int p){
     for(auto i:adj[u]){
         if(i==p)continue;
         up[i][0]=u;
         for(int j=1;j<l;j++){
             up[i][j]=up[up[i][j-1]][j-1];
         }
-        dfs(i,u,d+1);
+        depth[i]=depth[u]+1;
+        dfs(i,u);
     }
 }
-void burn() {
-    int n,q;cin>>n>>q;
-    l=int(log2(n))+2;
-    up=vector<vector<int>>(n+1,vector<int>(l,0));
-    depth=vector<int>(n+1,0);
-    for(int i=2;i<=n;i++){
-        int u;cin>>u;
-        adj[u].push_back(i);
+int get_LCA(int u,int v){
+    if(depth[u]<depth[v])swap(u,v);
+    int k=depth[u]-depth[v];
+    for(int i=0;i<l;i++){
+        if(k&(1<<i)){
+            u=up[u][i];
+        }
     }
-    dfs(1,1,0);
-    while (q--){
-      int u,v;cin>>u>>v;
-      if(depth[u]<depth[v])swap(u,v);
-      int k=depth[u]-depth[v];
-      for(int i=0;i<l;i++){
-          if(k&(1<<i)){
-              u=up[u][i];
-          }
-      }
-      if(u==v){
-          cout<<u<<'\n';
-          continue;
-      }
-      for(int i=l-1;i>=0;i--){
-          if(up[u][i]!=up[v][i]){
-              u=up[u][i];
-              v=up[v][i];
-          }
-      }
-      cout<<up[u][0]<<'\n';
+    if(u==v){
+        return u;
     }
- 
+    for(int i=l-1;i>=0;i--){
+        if(up[u][i]!=up[v][i]){
+            u=up[u][i];
+            v=up[v][i];
+        }
+    }
+    return up[u][0];
 }

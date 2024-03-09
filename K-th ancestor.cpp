8e598a72d8544f548323a429c34f851789106465
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-03-09 21:50:31 +0200

    Update K-th ancestor.cpp

--- K-th ancestor.cpp
+++ K-th ancestor.cpp
@@ -12,37 +12,14 @@ void dfs(int u,int p,int d){
         dfs(i,u,d+1);
     }
 }
-void burn() {
-int n,q;cin>>n>>q;
-l=int(log2(n))+2;
-up=vector<vector<int>>(n+1,vector<int>(l,0));
-depth=vector<int>(n+1,0);
-for(int i=2;i<=n;i++){
-    int u;cin>>u;
-    adj[u].push_back(i);
-}
-dfs(1,1,0);
-    while (q--){
-        int u,k;cin>>u>>k;
-        if(depth[u]<k){
-            cout<<"-1\n";
-            continue;
-        }
-        for(int i=0;i<l;i++){
-            if(k&(1<<i)){
-                u=up[u][i];
-            }
-        }
-        cout<<u<<'\n';
+int K_ANC(int u,int k){
+    if(depth[u]<k){
+        return -1;
     }
-
-}
-signed main() {
-    ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
-    int T = 1;//cin>>T;
-    while (T--) {
-        burn();
-        cout << '\n';
+    for(int i=0;i<l;i++){
+        if(k&(1<<i)){
+            u=up[u][i];
+        }
     }
-    return 0;
+    return u;
 }

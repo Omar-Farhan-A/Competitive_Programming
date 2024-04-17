31479c3071e0fafb5a10cb275882c869b4b165d6
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-04-17 19:40:59 +0200

    Update Segment_Tree.cpp

--- Segment_Tree.cpp
+++ Segment_Tree.cpp
@@ -1,41 +1,56 @@
-vector<int>seg(4 * N);
-vector<int>nums;
-int merge(int a,int  b){
-    return a + b;
-}
-void build(int idx,int lx,int rx){
-    if(lx==rx){
-        seg[idx]= nums[lx - 1];
-        return;
+struct Segment_Tree {
+    vector<ll> seg;
+    vector<int> nums;
+    int siz=1;
+    Segment_Tree(int n,vector<int>&nums){
+        while (siz<=n)siz*=2;
+        seg=vector<ll>(2*siz);
+        this->nums=nums;
+        build(1,1,siz,1,n);
     }
-    int mid=(lx+rx)>>1;
-    build(2*idx,lx,mid);
-    build(2*idx+1,mid+1,rx);
-    seg[idx]=merge(seg[2 * idx], seg[2 * idx + 1]);
-}
-int query(int idx,int lx,int rx,int l,int r){
-    if(lx>=l&&rx<=r){
-        return seg[idx];
+    ll merge(ll a, ll b) {
+        return a + b;
     }
-    int mid=(lx+rx)>>1;
-    if(l<=mid&&r>mid) {
-        return merge(query(idx * 2, lx, mid, l, r), query(idx * 2+1, mid + 1, rx, l, r));
+    void build(int idx, int lx, int rx,int l,int r) {
+        if (lx > r || rx < l)return;
+        if (lx == rx) {
+            seg[idx] = nums[lx - 1];
+            return;
+        }
+        int mid = (lx + rx) >> 1;
+        build(2 * idx, lx, mid,l,r);
+        build(2 * idx + 1, mid + 1, rx,l,r);
+        seg[idx] = merge(seg[2 * idx], seg[2 * idx + 1]);
     }
-    else if(r<=mid){
-        return query(idx * 2, lx, mid, l, r);
+
+    ll query(int idx, int lx, int rx, int l, int r) {
+        if (lx >= l && rx <= r) {
+            return seg[idx];
+        }
+        int mid = (lx + rx) >> 1;
+        if (l <= mid && r > mid) {
+            return merge(query(idx * 2, lx, mid, l, r), query(idx * 2 + 1, mid + 1, rx, l, r));
+        } else if (r <= mid) {
+            return query(idx * 2, lx, mid, l, r);
+        } else {
+            return query(idx * 2 + 1, mid + 1, rx, l, r);
+        }
     }
-    else {
-        return query(idx * 2+1, mid + 1, rx, l, r);
+    ll query(int l,int r){
+        return query(1,1,siz,l,r);
     }
-}
-void update(int idx,int lx,int rx,int i,int v){
-    if(lx>i||rx<i)return;
-    if(lx==rx){
-        seg[idx]=v;
-        return;
+    void update(int idx, int lx, int rx, int l, int r, ll v) {
+        if (lx > r || rx < l)return;
+        if (lx >= l && rx <= r) {
+            seg[idx] = v;
+            return;
+        }
+        int mid = (lx + rx) >> 1;
+        update(2 * idx, lx, mid, l, r, v);
+        update(2 * idx + 1, mid + 1, rx, l, r, v);
+        seg[idx] = merge(seg[2 * idx], seg[2 * idx + 1]);
     }
-    int mid=(lx+rx)>>1;
-    update(2*idx,lx,mid,i,v);
-    update(2*idx+1,mid+1,rx,i,v);
-    seg[idx]= merge(seg[2 * idx], seg[2 * idx + 1]);
-}
+    void update(int l,int r,int v){
+        update(1,1,siz,l,r,v);
+    }
+};

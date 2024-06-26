6243336c239c521629a4817eaaae0e16be947dc6
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-06-26 17:04:15 +0300

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,19 +1,25 @@
-int Parent[N],size[N];
-int getRoot(int u){
-    if(u == Parent[u])return u;
-    return Parent[u]= getRoot(Parent[u]);
-}
-bool sameSet(int u,int v){
-    return getRoot(u)== getRoot(v);
-}
-void merge(int u,int v){
-    u= getRoot(u);
-    v= getRoot(v);
-    if(u==v)
-        return;
-    Parent[u]=v;
-    size[v]+=size[u];
-}
-void ini(){
-    for(int i=1;i<N;i++)Parent[i]=i;
-}
+  struct DSU{
+    private:
+        vector<int>par,size;
+    public:
+        DSU(int n){
+            par.resize(n+5);
+            size.resize(n+5,1);
+            std::iota(par.begin(), par.end(),0);
+        }
+        int getRoot(int u){
+            if(u == par[u])return u;
+            return par[u]= getRoot(par[u]);
+        }
+        bool sameSet(int u,int v){
+            return getRoot(u)== getRoot(v);
+        }
+        void merge(int u,int v){
+            u= getRoot(u);
+            v= getRoot(v);
+            if(u==v)
+                return;
+            par[u]=v;
+            size[v]+=size[u];
+        }
+    };

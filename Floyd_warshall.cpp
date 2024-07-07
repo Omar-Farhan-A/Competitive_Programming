67d47c2c69a54256c83400eb13c75c2977bc1a6b
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-07-07 04:30:09 +0300

    Create Floyd_warshall.cpp

---
+++ Floyd_warshall.cpp
@@ -0,0 +1,11 @@
+vector<vector<ll>>dp;
+int n;
+void floyd() {
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+            }
+        }
+    }
+}

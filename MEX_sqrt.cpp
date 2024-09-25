fd5602df5d3467f5458457c9ddee371f07a14a36
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-09-26 00:15:58 +0300

    Create MEX_sqrt.cpp

---
+++ MEX_sqrt.cpp
@@ -0,0 +1,39 @@
+template <int N = (int)2e5>
+class MEX {
+private:
+    int SQ;
+    vector<int> block, freq;
+ 
+public:
+    MEX() {
+        SQ = sqrt(N + 1) + 1;
+        freq.assign(N + 1, 0);
+        block.assign(SQ, 0);
+    }
+ 
+    void add(int x) {
+        if (!freq[x]) {
+            block[x / SQ]--;
+        }
+        freq[x]++;
+    }
+ 
+    void remove(int x) {
+        freq[x]--;
+        if (!freq[x]) {
+            block[x / SQ]++;
+        }
+    }
+ 
+    int get_mex() {
+        for (int i = 0; i < SQ; ++i) {
+            if (block[i] != SQ) {
+                for (int j = i * SQ; j < i * SQ + SQ; ++j) {
+                    if (!freq[j]) {
+                        return j;
+                    }
+                }
+            }
+        }
+    }
+};

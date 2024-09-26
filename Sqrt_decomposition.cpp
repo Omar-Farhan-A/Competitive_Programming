b413604f6125b27de17cddee12f92e6a4342ccb6
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-09-26 05:15:10 +0300

    Update Sqrt_decomposition.cpp

--- Sqrt_decomposition.cpp
+++ Sqrt_decomposition.cpp
@@ -1,38 +1,40 @@
 struct Sqrt_decomposition {
-        // 0-based
+    // 0-based
 private:
-        vector<ll>v,b;
-        int SQ,n;
+    vector<ll> v, b;
+    int SQ, n;
 public:
-        Sqrt_decomposition(int n,vector<ll>&a){
-            v=a;
-            SQ=int(sqrt(n+.0))+1;
-            b=vector<ll>(SQ,0);
-            this->n=n;
-            build();
+    Sqrt_decomposition(int n, vector<ll> &a) {
+        v = a;
+        SQ = int(sqrt(n + .0)) + 1;
+        b = vector<ll>(SQ, 0);
+        this->n = n;
+        build();
+    }
+
+    void build() {
+        for (int i = 0; i < n; i++) {
+            b[i / SQ] += v[i];
         }
-        void build(){
-            for(int i=0;i<n;i++){
-                b[i/SQ]+=v[i];
+    }
+
+    ll Query(int l, int r) {
+        ll ret = 0;
+        for (int i = l; i <= r;) {
+            if (i % SQ == 0 && i + SQ - 1 <= r) {
+                ret += b[i / SQ];
+                i += SQ;
+            } else {
+                ret += v[i];
+                i++;
             }
         }
-        ll Query(int l,int r){
-            ll ret=0;
-            for(int i=l;i<=r;){
-                if(i%SQ==0&&i+SQ-1<=r){
-                    ret+=b[i/SQ];
-                    i+=SQ;
-                }
-                else {
-                    ret+=v[i];
-                    i++;
-                }
-            }
-            return ret;
-        }
-        void update(int idx,ll val){
-            b[idx/SQ]-=v[idx];
-            v[idx]=val;
-            b[idx/SQ]+=v[idx];
-        }
-    };
+        return ret;
+    }
+
+    void update(int idx, ll val) {
+        b[idx / SQ] -= v[idx];
+        v[idx] = val;
+        b[idx / SQ] += v[idx];
+    }
+};

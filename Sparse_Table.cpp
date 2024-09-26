7e9391188710a34fa1c63fff528d004715a856d6
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-09-26 03:34:12 +0300

    Update Sparse_Table.cpp

--- Sparse_Table.cpp
+++ Sparse_Table.cpp
@@ -1,34 +1,33 @@
-struct SparseTable{
-//0-based
-        int LOG,n;
-        vector<vector<int>>S;
-        vector<int>bin_log;
-        vector<int>a;
-        SparseTable(int n,vector<int>&a){
-            LOG=20;
-            S=vector<vector<int>>(n,vector<int>(LOG));
-            bin_log.resize(n+5);
-            this->a=a;
-            this->n=n;
-            build();
+struct Sparse_table {
+    vector<vector<int>> S;
+    vector<int> bin_log, a;
+    int n;
+
+    void build() {
+        int cur_log = 0;
+        for (int i = 0; i < n; i++) {
+            S[i][0] = a[i];
+            if ((1 << (cur_log + 1)) <= i + 1)cur_log++;
+            bin_log[i + 1] = cur_log;
         }
-        void build(){
-            int cur_log=0;
-            for(int i=0;i<n;i++){
-                S[i][0]=a[i];
-                while ((1<<(cur_log+1))<=i+1){
-                    cur_log++;
-                }
-                bin_log[i+1]=cur_log;
-            }
-            for(int k=1;k<LOG;k++){
-                for(int i=0;i+(1<<(k))-1< n;i++){
-                    S[i][k]=gcd(S[i][k-1],S[i+(1<<(k-1))][k-1]);
-                }
+        for (int k = 1; k < 20; k++) {
+            for (int i = 0; i + (1 << k) - 1 < n; i++) {
+                S[i][k] = min(S[i][k - 1], S[i + (1 << (k - 1))][k - 1]);
             }
         }
-    int query(int L,int R){
-        int k=bin_log[R-L+1];
-        return gcd(S[L][k],S[R-(1<<k)+1][k]);
     }
-    };
+
+    int query(int L, int R) {
+        int k = bin_log[R - L + 1];
+        return min(S[L][k], S[R - (1 << k) + 1][k]);
+    }
+
+    Sparse_table(int n, vector<int> &a) {
+        S = vector<vector<int>>(n, vector<int>(20));
+        bin_log.resize(n + 5);
+        this->a = a;
+        this->n = n;
+        build();
+    }
+
+};

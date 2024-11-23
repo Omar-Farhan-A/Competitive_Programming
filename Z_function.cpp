855e353057e21b9c5c7dd848ccd4e538304c2a2c
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-11-23 21:56:23 +0200

    Update Z_function.cpp

--- Z_function.cpp
+++ Z_function.cpp
@@ -1,17 +1,17 @@
-vector<int> Z_function(string s){
-    int n=s.size();
-    vector<int>z(n);
-    int l=0,r=0;
-    for(int i=1;i<n;i++){
-        if(i<r){
-            z[i]=min(z[i-l],r-i);
+vector<int> Z_function(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; i++) {
+        if (i < r) {
+            z[i] = min(r - i, z[i - l]);
         }
-        while (i+z[i]<n&&s[z[i]]==s[z[i]+i]){
+        while (i + z[i] < n && s[z[i]] == s[z[i] + i]) {
             z[i]++;
         }
-        if(i+z[i]>r){
-            r=i+z[i];
-            l=i;
+        if (i + z[i] > r) {
+            r = i + z[i];
+            l = i;
         }
     }
     return z;

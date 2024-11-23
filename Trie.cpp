f62d09c8fee4f7be94c55778536c7f574beed870
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-11-23 21:38:19 +0200

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,45 +1,55 @@
-struct Trie{
-        struct Node{
-            Node *child[26];
-            int prefix,end;
-            Node(){
-                prefix=end=0;
-                memset(child, 0, sizeof child);
-            }
-        };
-        Node *root=new Node();
-        void insert(string &s){
-            Node *cur=root;
-            for(auto it:s){
-                int idx=it-'a';
-                if(cur->child[idx]==0){
-                    cur->child[idx]=new Node();
-                }
-                cur=cur->child[idx];
-                cur->prefix++;
-            }
-            cur->end++;
-        }
-        int cnt_prefix(string &s){
-            Node *cur=root;
-            for(auto it:s){
-                int idx=it-'a';
-                if(cur->child[idx]==0){
-                    return 0;
-                }
-                cur=cur->child[idx];
-            }
-            return cur->prefix;
-        }
-        int cnt_word(string &s){
-            Node *cur=root;
-            for(auto it:s){
-                int idx=it-'a';
-                if(cur->child[idx]==0){
-                    return 0;
-                }
-                cur=cur->child[idx];
-            }
-            return cur->end;
+class Trie {
+    struct Node {
+        int next[26], prefix, end;
+
+        Node() {
+            memset(next, -1, sizeof next);
+            prefix = end = 0;
         }
     };
+
+    vector<Node> trie;
+public:
+
+    Trie() {
+        trie = vector<Node>(1);
+    }
+
+    void insert(string &s) {
+        int v = 0;
+        for (auto &ch: s) {
+            int c = ch - 'a';
+            if (trie[v].next[c] == -1) {
+                trie[v].next[c] = int(trie.size());
+                trie.emplace_back();
+            }
+            v = trie[v].next[c];
+            trie[v].prefix++;
+        }
+        trie[v].end++;
+    }
+
+    int cnt_word(string &s) {
+        int v = 0;
+        for (auto &ch: s) {
+            int c = ch - 'a';
+            if (trie[v].next[c] == -1) {
+                return 0;
+            }
+            v = trie[v].next[c];
+        }
+        return trie[v].end;
+    }
+
+    int cnt_prefix(string &s) {
+        int v = 0;
+        for (auto &ch: s) {
+            int c = ch - 'a';
+            if (trie[v].next[c] == -1) {
+                return 0;
+            }
+            v = trie[v].next[c];
+        }
+        return trie[v].prefix;
+    }
+};

ff1a6ed2f71970f6a6d3bb65a3bcb00169b150e3
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2024-11-24 17:57:48 +0200

    Update BinaryTrie.cpp

--- BinaryTrie.cpp
+++ BinaryTrie.cpp
@@ -1,46 +1,50 @@
-struct BinaryTrie{
-        struct Node{
-            Node *child[2];
-            int fre[2];
-            Node(){
-                child[0]=child[1]=0;
-                fre[0]=fre[1]=0;
-            }
-        };
-        Node *root =new Node();
-        void insert(int x){
-            Node *cur=root;
-            for(int i=30;i>=0;i--){
-               bool idx=(x>>i)&1;
-               if(cur->child[idx]==0)cur->child[idx]=new Node();
-                cur->fre[idx]++;
-                cur=cur->child[idx];
-            }
-        }
-        void del(int n,int i,Node *cur){
-            if(i==-1)return;
-            bool idx=(n>>i)&1;
-            del(n,i-1,cur->child[idx]);
-            cur->fre[idx]--;
-            if(cur->fre[idx]==0)
-            {
-                delete cur->child[idx];
-                cur->child[idx]=0;
-            }
-        }
-        int query(int x){
-            int ret=0;
-            Node *cur=root;
-            for(int i=30;i>=0;i--){
-                int idx=(x>>i)&1;
-                if(cur->child[idx^1]==0){
-                    cur=cur->child[idx];
-                }
-                else {
-                    ret|=(1<<i);
-                    cur=cur->child[idx^1];
-                }
-            }
-            return ret;
+class BinaryTrie {
+    struct Node {
+        int next[2], fre[2];
+
+        Node() {
+            memset(next, -1, sizeof next);
+            fre[0] = fre[1] = 0;
         }
     };
+
+    vector<Node> trie;
+public:
+
+    BinaryTrie() {
+        trie = vector<Node>(1);
+    }
+
+    void update(ll x, int op = 1) {
+        int v = 0;
+        for (int i = 63; ~i; i--) {
+            bool on = (x >> i) & 1;
+            if (!trie[v].fre[on]) {
+                trie[v].next[on] = int(trie.size());
+                trie.emplace_back();
+            }
+            trie[v].fre[on] += op;
+            v = trie[v].next[on];
+        }
+    }
+
+    ll query_max(ll x) {
+        int v = 0;
+        ll ans = 0;
+        for (int i = 63; ~i; i--) {
+            bool on = (x >> i) & 1;
+            if (trie[v].fre[!on]) {
+                ans |= (1ll << i);
+                v = trie[v].next[!on];
+            } else {
+                v = trie[v].next[on];
+            }
+        }
+        return ans;
+    }
+
+    ll query_min(ll x) {
+        // must iterate over all bits
+        return -1ll ^ query_max(-1ll ^ x);
+    }
+};

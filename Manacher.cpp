362b02a9b249c903ad3fa23bd6996421153f0b44
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2025-01-03 07:27:18 +0200

    Update Manacher.cpp

--- Manacher.cpp
+++ Manacher.cpp
@@ -1,24 +1,45 @@
-vector<int> manacher_odd(string s) {
-    int n = s.size();
-    s = "$" + s + "^";
-    vector<int> p(n + 2);
-    int l = 1, r = 1;
-    for(int i = 1; i <= n; i++) {
-        p[i] = max(0, min(r - i, p[l + (r - i)]));
-        while(s[i - p[i]] == s[i + p[i]]) {
-            p[i]++;
+class Manacher {
+private:
+    vector<int> p;
+public:
+    Manacher(string &s) {
+        string t;
+        for (auto &c: s) {
+            t += string("#") + c;
         }
-        if(i + p[i] > r) {
-            l = i - p[i], r = i + p[i];
+        t += '#';
+        auto res = manacher_odd(t);
+        p = vector<int>(begin(res) + 1, end(res));
+    }
+
+    vector<int> manacher_odd(string s) {
+        int n = int(s.size());
+        s = "$" + s + "^";
+        vector<int> v(n + 2);
+        int l = 1, r = 1;
+        for (int i = 1; i <= n; i++) {
+            v[i] = max(0, min(r - i, v[l + (r - i)]));
+            while (s[i - v[i]] == s[i + v[i]]) {
+                v[i]++;
+            }
+            if (i + v[i] > r) {
+                r = i + v[i];
+                l = i - v[i];
+            }
         }
+        return vector<int>(begin(v) + 1, end(v) - 1);
     }
-    return vector<int>(begin(p) + 1, end(p) - 1);
-}
-vector<int> manacher(string s) {
-    string t;
-    for(auto c: s) {
-        t += string("#") + c;
+
+    int get_odd(int i) {
+        return p[i << 1] / 2;
     }
-    auto res = manacher_odd(t + "#");
-    return vector<int>(begin(res) + 1, end(res) - 1);
-}
+
+    int get_even(int i) {
+        return p[i << 1 | 1] / 2;
+    }
+
+    int get(int i) {
+        return get_even(i) + get_odd(i);
+    }
+
+};

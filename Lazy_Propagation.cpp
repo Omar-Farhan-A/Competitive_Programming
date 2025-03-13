116dc2f6265976a10f3a86d50618395053c2aa13
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2025-03-14 00:25:53 +0200

    Update Lazy_Propagation.cpp

--- Lazy_Propagation.cpp
+++ Lazy_Propagation.cpp
@@ -1,72 +1,80 @@
-struct Lazy_Propagation {
-    vector<ll> seg;
-    vector<ll> lazy;
-    vector<int> nums;
-    int siz=1;
-    Lazy_Propagation(int n,vector<int>&nums){
-        while (siz<=n)siz*=2;
-        seg=vector<ll>(2*siz);
-        lazy=vector<ll>(2*siz);
-        this->nums=nums;
-        build(1,1,siz,1,n);
-    }
-    void propagate(int idx, int lx, int rx) {
-        if (!lazy[idx])return;
-        seg[idx] += lazy[idx]*(rx-lx+1);
-        if (lx != rx) {
-            lazy[idx * 2] += lazy[idx];
-            lazy[idx * 2 + 1] += lazy[idx];
+template<typename T=int>
+class Segment_Tree {
+    struct Node {
+        T val, lazy;
+
+        Node(T v = 0) {
+            val = v;
+            lazy = 0;
         }
-        lazy[idx] = 0;
+
+        void merge(Node a, Node b) {
+            val = a.val + b.val;
+        }
+    };
+
+    vector<Node> sg;
+    int size;
+
+    void propagate(int idx, int lx, int rx) {
+        if (!sg[idx].lazy)return;
+        sg[idx].val += sg[idx].lazy * (rx - lx + 1);
+        int left = idx << 1, right = idx << 1 | 1;
+        if (lx != rx) {
+            sg[left].lazy += sg[idx].lazy;
+            sg[right].lazy += sg[idx].lazy;
+        }
+        sg[idx].lazy = 0;
     }
 
-    ll merge(ll a, ll b) {
-        return a + b;
-    }
-    void build(int idx, int lx, int rx,int l,int r) {
-        if (lx > r || rx < l)return;
-        propagate(idx, lx, rx);
+    void build(int idx, int lx, int rx, vector<int> &a) {
         if (lx == rx) {
-            seg[idx] = nums[lx - 1];
+            sg[idx] = (lx < a.size() ? a[lx] : 0);
             return;
         }
-        int mid = (lx + rx) >> 1;
-        build(2 * idx, lx, mid,l,r);
-        build(2 * idx + 1, mid + 1, rx,l,r);
-        seg[idx] = merge(seg[2 * idx], seg[2 * idx + 1]);
+        int m = (lx + rx) >> 1, left = idx << 1, right = idx << 1 | 1;
+        build(left, lx, m, a);
+        build(right, m + 1, rx, a);
+        sg[idx].merge(sg[left], sg[right]);
     }
 
-    ll query(int idx, int lx, int rx, int l, int r) {
-        propagate(idx, lx, rx);
-        if (lx >= l && rx <= r) {
-            return seg[idx];
-        }
-        int mid = (lx + rx) >> 1;
-        if (l <= mid && r > mid) {
-            return merge(query(idx * 2, lx, mid, l, r), query(idx * 2 + 1, mid + 1, rx, l, r));
-        } else if (r <= mid) {
-            return query(idx * 2, lx, mid, l, r);
-        } else {
-            return query(idx * 2 + 1, mid + 1, rx, l, r);
-        }
-    }
-    ll query(int l,int r){
-        return query(1,1,siz,l,r);
-    }
-    void update(int idx, int lx, int rx, int l, int r, ll v) {
+    void update(int idx, int lx, int rx, int l, int r, T val) {
         propagate(idx, lx, rx);
         if (lx > r || rx < l)return;
-        if (lx >= l && rx <= r) {
-            lazy[idx] += v;
+        if (l <= lx && rx <= r) {
+            sg[idx].lazy += val;
             propagate(idx, lx, rx);
             return;
         }
-        int mid = (lx + rx) >> 1;
-        update(2 * idx, lx, mid, l, r, v);
-        update(2 * idx + 1, mid + 1, rx, l, r, v);
-        seg[idx] = merge(seg[2 * idx], seg[2 * idx + 1]);
+        int m = (lx + rx) >> 1, left = idx << 1, right = idx << 1 | 1;
+        update(left, lx, m, l, r, val);
+        update(right, m + 1, rx, l, r, val);
+        sg[idx].merge(sg[left], sg[right]);
     }
-    void update(int l,int r,int v){
-        update(1,1,siz,l,r,v);
+
+    T query(int idx, int lx, int rx, int l, int r) {
+        if (lx > r || rx < l)return 0;
+        propagate(idx, lx, rx);
+        if (l <= lx && rx <= r) {
+            return sg[idx].val;
+        }
+        int m = (lx + rx) >> 1, left = idx << 1, right = idx << 1 | 1;
+        return query(left, lx, m, l, r) + query(right, m + 1, rx, l, r);
+    }
+
+public:
+    Segment_Tree(vector<int> &a) {
+        size = 1;
+        while (size < a.size())size <<= 1;
+        sg.resize(size << 1);
+        build(1, 0, size - 1, a);
+    }
+
+    T query(int l, int r) {
+        return query(1, 0, size - 1, l, r);
+    }
+
+    void update(int l, int r, T val) {
+        update(1, 0, size - 1, l, r, val);
     }
 };

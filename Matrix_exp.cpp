73eac846db7604a0a2c06331bf44d132f3cc0f54
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2025-04-02 13:03:09 +0200

    Update Matrix_exp.cpp

--- Matrix_exp.cpp
+++ Matrix_exp.cpp
@@ -1,35 +1,37 @@
-#define vvl vector<vector<ll>>
-vvl IDN={{1,0},{0,1}};
-vvl mulMat(vvl &a,vvl &b){
-    int rows=int(a.size());
-    int cols=int(b[0].size());
-    vvl ret(rows,vector<ll>(cols));
-    int K=int(b.size());
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<cols;j++){
-            for(int k=0;k<K;k++){
-                (ret[i][j]+=a[i][k] * b[k][j])%=MOD;
+struct Matrix {
+    vector<vector<ll>> a;
+ 
+    Matrix(vector<vector<ll>> init = {}) {
+        a = init;
+    }
+ 
+    Matrix operator*(Matrix other) {
+        int n = a.size(), m = other.a[0].size(), g = other.a.size();
+        vector<vector<ll>> product(n, vector<ll>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < g; k++) {
+                    (product[i][j] += a[i][k] * other.a[k][j]) %= MOD;
+                }
             }
         }
+        return product;
     }
-    return ret;
-}
-vvl fastExp(vvl &x,ll n){
-    vvl product=IDN;
-    while(n > 0) {
-        if(n % 2) {
-            product = mulMat( product , x);
+};
+ 
+Matrix fast_exp(Matrix m, ll p) {
+    int n = m.a.size();
+    vector<vector<ll>> init(n, vector<ll>(n));
+    Matrix product(init);
+    for (int i = 0; i < n; i++) {
+        product.a[i][i] = 1;
+    }
+    while (p) {
+        if (p & 1) {
+            product = product * m;
         }
-        x = mulMat(x , x);
-        n >>=1;
+        m = m * m;
+        p >>= 1;
     }
     return product;
-}
-void burn() {
-    ll n;cin>>n;
-    vvl trans={{1,1},{1,0}};
-    vvl fib={{1,0}};
-    vvl v= fastExp(trans,n);
-    vvl ans= mulMat(v,fib);
-    cout<<ans[1][0];
 }

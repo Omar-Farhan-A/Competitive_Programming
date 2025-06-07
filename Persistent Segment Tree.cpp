79afee4ee56018b7b8b358595c69c0e7da20a391
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2025-06-07 09:04:04 +0300

    Update Persistent.cpp

--- Persistent Segment Tree.cpp
+++ Persistent Segment Tree.cpp
@@ -1,81 +1,64 @@
-template<typename T=int>
-class Persistent_Segment_Tree {
-public:
-    struct Node {
-        int left, right;
-        T val;
+struct Node {
+    ll val{};
+    Node *left, *right;
 
-        Node() {
-            val = left = right = 0;
-        }
-    };
+    Node() {}
 
-private:
-    vector<Node> sg;
-    vector<int> roots;
-    int Nodes = 0, siz;
-public:
-    Persistent_Segment_Tree(int n, vector<int> a) {
-        siz = 1;
-        while (siz <= n)siz <<= 1;
-        sg.resize(siz * 20);
-        roots.push_back(build(1, siz, a));
+    Node(int val) : val(val) {}
 
+    Node(Node *l, Node *r) {
+        left = l;
+        right = r;
+        if (left)val += left->val;
+        if (right)val += right->val;
     }
+};
 
-    T merge(T a, T b) {
-        return a + b;
-    }
+struct PS {
+    int n;
+    vector<Node *> roots;
 
-    int build(int lx, int rx, vector<int> &a) {
-        int curNode = Nodes++;
-        if (lx == rx) {
-            if (lx <= a.size())
-                sg[curNode].val = a[lx - 1];
-            return curNode;
-        }
+    Node *build(int lx, int rx, vector<int> &a) {
+        if (lx == rx)return new Node(a[lx]);
         int m = (lx + rx) >> 1;
-        sg[curNode].left = build(lx, m, a);
-        sg[curNode].right = build(m + 1, rx, a);
-        sg[curNode].val = merge(sg[sg[curNode].left].val, sg[sg[curNode].right].val);
-        return curNode;
+        auto left = build(lx, m, a);
+        auto right = build(m + 1, rx, a);
+        return new Node(left, right);
     }
 
-    T query(int node, int lx, int rx, int l, int r) {
-        if (r < lx || rx < l)return 0;
-        if (lx >= l && r >= rx)return sg[node].val;
+    Node *update(Node *node, int lx, int rx, int i, int x) {
+        if (lx == rx) return new Node(x);
         int m = (lx + rx) >> 1;
-        return merge(query(sg[node].left, lx, m, l, r), query(sg[node].right, m + 1, rx, l, r));
+        if (i <= m)
+            return new Node(update(node->left, lx, m, i, x), node->right);
+        else
+            return new Node(node->left, update(node->right, m + 1, rx, i, x));
     }
 
-    int update(int prevNode, int lx, int rx, int i, T val) {
-        int curNode = Nodes++;
-        if (lx == rx) {
-            sg[curNode].val = val;
-            return curNode;
-        }
-        // copy prev Node's values
-        sg[curNode] = sg[prevNode];
+    ll query(Node *node, int lx, int rx, int l, int r) {
+        if (lx > r || rx < l) return 0;
+        if (l <= lx && rx <= r) return node->val;
         int m = (lx + rx) >> 1;
-        if (i <= m) {
-            sg[curNode].left = update(sg[prevNode].left, lx, m, i, val);
-        } else {
-            sg[curNode].right = update(sg[prevNode].right, m + 1, rx, i, val);
-        }
-        sg[curNode].val = merge(sg[sg[curNode].left].val, sg[sg[curNode].right].val);
-        return curNode;
+        return query(node->left, lx, m, l, r) + query(node->right, m + 1, rx, l, r);
     }
 
-    T query(int l, int r, int k) {
-        return query(roots[k - 1], 1, siz, l, r);
+    PS(vector<int> &a) {
+        n = 1;
+        while (n < sz(a))n <<= 1;
+        n <<= 1;
+        roots.push_back(build(0, n - 1, a));
     }
 
-    void update(int k, int i, T val) {
-        roots[k - 1] = update(roots[k - 1], 1, siz, i, val);
+    void update(int k, int i, int x) {
+        roots[k] = update(roots[k], 0, n - 1, i, x);
+    }
+
+    ll query(int k, int l, int r) {
+        return query(roots[k], 0, n - 1, l, r);
     }
 
     void makeCopy(int k) {
-        roots.push_back(roots[k - 1]);
+        roots.push_back(roots[k]);
     }
 
 };

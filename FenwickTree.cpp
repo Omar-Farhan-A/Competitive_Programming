ae685ec1ad0ca8dcb7b7c13283545b9f1f219b3d
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2025-06-10 05:02:31 +0300

    Update FenwickTree.cpp

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -1,28 +1,31 @@
-struct FenwickTree{
+struct FenwickTree {
 private:
-        vector<ll>tree;
-        int N;
+    vector<ll> tree;
+    int N;
 public:
-        FenwickTree(int n){
-            N=n;
-            tree.resize(N+5);
-        }
-        void add(int idx,ll val){
-            while (idx<=N){
-                tree[idx]+=val;
-                idx+=idx&-idx;
-            }
-        }
-        ll query(int idx){
-            ll ret=0;
-            while (idx>0){
-                ret+=tree[idx];
-                idx-=idx&-idx;
-            }
-            return ret;
-        }
-        ll query(int l,int r){
-            return query(r)- query(l-1);
-        }
+    FenwickTree(int n) {
+        N = n;
+        tree.resize(N + 5);
+    }
 
-    };
+    void add(int idx, ll val) {
+        while (idx <= N) {
+            tree[idx] += val;
+            idx += idx & -idx;
+        }
+    }
+
+    ll query(int idx) {
+        ll ret = 0;
+        while (idx > 0) {
+            ret += tree[idx];
+            idx -= idx & -idx;
+        }
+        return ret;
+    }
+
+    ll query(int l, int r) {
+        return query(r) - query(l - 1);
+    }
+
+};

1a2718cda81d57ec021981a020e915b1a41a70ab
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2025-07-29 05:52:58 +0300

    Update Bellman-Ford.cpp

--- Graph/Bellman-Ford.cpp
+++ Graph/Bellman-Ford.cpp
@@ -1,20 +1,20 @@
-struct Edge
-{
-    int u,v,w;
+struct Edge {
+    int u, v, w;
 };
-vector<Edge>adj;
-ll Bellmanford(int src,int n){
-    vector<ll>dist(n+5,LINF);
-    dist[src]=0;
-     bool ok=false;
-    for(int i=0;i<n;i++){
-        for(auto &[u,v,w]:adj){
-            if(dist[v]>dist[u]+w){
-                dist[v]=dist[u]+w;
-                if(v==n&&i==n-1)ok=true;
+vector<Edge> adj;
+
+ll Bellmanford(int src, int n) {
+    vector<ll> dist(n + 5, 1e15);
+    dist[src] = 0;
+    bool ok = false;
+    for (int i = 0; i < n; i++) {
+        for (auto &[u, v, w]: adj) {
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                if (v == n && i == n - 1)ok = true;
             }
         }
     }
-    if(ok)return -LINF;//negative cycle
+    if (ok)return -1e15;//negative cycle
     return dist[n];
 }

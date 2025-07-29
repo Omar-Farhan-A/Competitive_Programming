79fb28849dd7d839a84181f0b742402b4644ff09
Omar Farhan <149535765+Omar-Farhan-A@users.noreply.github.com>
2025-07-29 05:44:45 +0300

    Update tarjan.cpp

--- Graph/tarjan.cpp
+++ Graph/tarjan.cpp
@@ -1,27 +1,26 @@
-vector< vector<int> > adj, components;
-vector<int> inStack, lowLink,compNum, inDeg;
+vector<vector<int> > adj, components;
+vector<int> inStack, lowLink, compNum, inDeg;
 stack<int> stk;
 int timer;
+
 void tarjan(int u) {
-    lowLink[u]=inDeg[u]=++timer,inStack[u]=1;
+    lowLink[u] = inDeg[u] = ++timer, inStack[u] = 1;
     stk.push(u);
-    for(auto &i:adj[u]){
-        if(!inDeg[i]){
-        tarjan(i);
-        lowLink[u]=min(lowLink[u],lowLink[i]);
-        }
-        else if(inStack[i]){
-        lowLink[u]=min(lowLink[u],inDeg[i]);
+    for (auto &i: adj[u]) {
+        if (!inDeg[i]) {
+            tarjan(i);
+            lowLink[u] = min(lowLink[u], lowLink[i]);
+        } else if (inStack[i]) {
+            lowLink[u] = min(lowLink[u], inDeg[i]);
         }
     }
-    if(inDeg[u]==lowLink[u]){
+    if (inDeg[u] == lowLink[u]) {
         components.push_back(vector<int>());
-        int x=-1;
-        while (x!=u)
-        {
-            x=stk.top(),stk.pop(),inStack[x]=0;
+        int x = -1;
+        while (x != u) {
+            x = stk.top(), stk.pop(), inStack[x] = 0;
             components.back().push_back(x);
-            compNum[x]=components.size();
+            compNum[x] = components.size();
         }
     }
 }
